7d215086ceb9054ac3388c2552df6ad06ca523c6
zengmingqi <1475927880@qq.com>
2020-10-13 18:56:23 +0800

    1、增加事务配置

---
+++ server/src/main/java/com/course/server/config/TransactionManagerConfig.java
@@ -0,0 +1,13 @@
+package com.course.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author zmq
+ * @date 2020/10/13 6:49 下午
+ */
+@Configuration
+@EnableTransactionManagement
+public class TransactionManagerConfig {
+}
