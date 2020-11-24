3e6de403db091b41aeecd7b182e3e0e12f2053c7
zengmingqi <1475927880@qq.com>
2020-11-24 11:01:19 +0800

    注册中心增加密码

--- eureka/src/main/java/com/course/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/course/eureka/EurekaApplication.java
@@ -3,11 +3,23 @@ package com.course.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            super.configure(http);
+            http.csrf().disable();
+        }
     }
 }
